e1927d7701e9dce85ce5c79c21b4156fb856eced
语文与数学 <13934779844@qq.com>
2023-03-02 17:33:57 +0800

    update: 新增文档

--- blog/js/promise.js
+++ blog/js/promise.js
@@ -5,26 +5,29 @@ class myPromise {
     constructor(fun) {
         this.PromiseStatus = myPromise.PEDDING
         this.PromiseResult = null
-        fun(this.resolve.bind(this),this.reject.bind(this))
+        fun(this.resolve.bind(this), this.reject.bind(this))
     }
     resolve(result) {
-        if(this.PromiseStatus = myPromise.PEDDING) {
+        if (this.PromiseStatus = myPromise.PEDDING) {
             this.PromiseStatus = myPromise.FULFILLED
             this.PromiseResult = result
         }
     }
     reject(reason) {
-        if(this.PromiseStatus = myPromise.REJECTED) {
+        if (this.PromiseStatus = myPromise.REJECTED) {
             this.PromiseStatus = myPromise.REJECTED
             this.PromiseResult = reason
         }
     }
-    then(onFulfiled,onRejcted) {
-        if(this.PromiseState  === myPromise.FULFILLED) {
-            onFulfiled(this.PromiseResult)
-        }
-        if(this.PromiseState  === myPromise.REJECTED) {
-            onRejcted(this.PromiseResult)
-        }
+    then(onFulfiled, onRejcted) {
+        setTimeout(() => {
+            if (this.PromiseState === myPromise.FULFILLED) {
+                onFulfiled(this.PromiseResult)
+            }
+            if (this.PromiseState === myPromise.REJECTED) {
+                onRejcted(this.PromiseResult)
+            }
+        })
+
     }
 }
